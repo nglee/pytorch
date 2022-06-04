14358536fe64918f948bd8f23af35d2cfa2217b9
Edward Z. Yang <ezyang@fb.com>
2022-06-03 20:06:23 -0700

    Replace TensorMeta with FakeTensor
    Signed-off-by: Edward Z. Yang <ezyangfb.com>
    
    ghstack-source-id: 42d53562323f8dab263ce60c32f0505b4ff8e1c3
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/78836

--- torch/_prims/context.py
+++ torch/_prims/context.py
@@ -7,8 +7,9 @@ import functools
 import torch
 from torch.fx.graph import Graph, Node
 import torch.overrides
+from torch._subclasses.fake_tensor import FakeTensor
 
-from torch._prims.utils import TensorMeta, torch_function_passthrough
+from torch._prims.utils import torch_function_passthrough
 import torch._refs as refs
 
 import torch._refs
@@ -90,25 +91,25 @@ class PrimContext(torch.overrides.TorchFunctionMode):
 
         return self._create_name(idx, self._lowercase)
 
-    def _add_user(self, tm: TensorMeta, node: Node) -> None:
-        assert tm.node is not None
-        tm.node.users[node] = None
+    def _add_user(self, tm: FakeTensor, node: Node) -> None:
+        assert tm.node is not None  # type: ignore[attr-defined]
+        tm.node.users[node] = None  # type: ignore[attr-defined]
 
     def placeholder(self, a: Any):
         name = self._tensor_name()
         node = self.graph.placeholder(name)
 
-        if isinstance(a, TensorMeta):
-            if a.node is not None:
-                raise ValueError("Attempting to reuse a TensorMeta in a new trace!")
-            a.tname = name
-            a.node = node
+        if isinstance(a, FakeTensor):
+            if hasattr(a, "node"):
+                raise ValueError("Attempting to reuse a FakeTensor in a new trace!")
+            a.tname = name  # type: ignore[attr-defined]
+            a.node = node  # type: ignore[attr-defined]
 
         return a
 
-    def output(self, tm: TensorMeta):
+    def output(self, tm: FakeTensor):
         # TODO: allow other output types
-        assert isinstance(tm, TensorMeta)
+        assert isinstance(tm, FakeTensor)
 
         node = self.graph.output(tm)
         self._add_user(tm, node)
@@ -141,17 +142,17 @@ class PrimContext(torch.overrides.TorchFunctionMode):
             # Updates graph
             # TODO: handle outputs with multiple tensors
             # TODO: handle non-tensor outputs
-            assert isinstance(output, TensorMeta)
+            assert isinstance(output, FakeTensor)
             output_name = self._tensor_name()
             node = self.graph.create_node(
                 "call_function", func, name=output_name, args=args, kwargs=kwargs
             )
-            output.tname = output_name
-            output.node = node
+            output.tname = output_name  # type: ignore[attr-defined]
+            output.node = node  # type: ignore[attr-defined]
 
             # Marks uses
             for x in (
-                x for x in chain(args, kwargs.values()) if isinstance(x, TensorMeta)
+                x for x in chain(args, kwargs.values()) if isinstance(x, FakeTensor)
             ):
                 self._add_user(x, node)
 
@@ -180,7 +181,7 @@ def torch_to_refs_map():
     ]
     r = {}
     for mod_torch, mod_refs in modules:
-        for s in mod_refs.__all__:
+        for s in mod_refs.__all__:  # type: ignore[attr-defined]
             r[mod_torch.__dict__.get(s)] = mod_refs.__dict__.get(s)
     return r
 
